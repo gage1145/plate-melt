b599cee1861ade85fdb5e6e2fdcc29e47b87096d
gage1145 <140093724+gage1145@users.noreply.github.com>
2026-02-04 10:23:18 -0600

    Added example script.

---
+++ bot.py
@@ -0,0 +1,20 @@
+import os
+import logging
+from signalbot import SignalBot, Command, Context, triggered, enable_console_logging
+
+
+class PingCommand(Command):
+    @triggered("Ping")
+    async def handle(self, c: Context) -> None:
+        await c.send("Pong")
+
+
+if __name__ == "__main__":
+    enable_console_logging(logging.INFO)
+
+    bot = SignalBot({
+        "signal_service": os.environ["SIGNAL_SERVICE"],
+        "phone_number": os.environ["PHONE_NUMBER"]
+    })
+    bot.register(PingCommand()) # Run the command for all contacts and groups
+    bot.start()
